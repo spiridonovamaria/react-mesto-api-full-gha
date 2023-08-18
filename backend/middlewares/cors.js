70ac7c6791e95851e5f5f8b7ab794272455732c3
Maria Spiridonova <traffic2007@mail.ru>
2023-08-19 01:11:37 +0300

    15

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,6 +1,8 @@
 const allowedCors = [
   'https://spiridon.nomoreparties.co',
   'http://spiridon.nomoreparties.co',
+  'https://api.spiridon.nomoreparties.co',
+  'http://api.spiridon.nomoreparties.co',
   'https://localhost:3000',
   'http://localhost:3000',
   'https://localhost:3001',
